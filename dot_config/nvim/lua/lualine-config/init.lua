c694f7981ef1a65ab1ba7f786c5065e5272707e2
Zach Yeap <zyeap@juniper.net>
2022-10-26 15:20:33 -0700

    dracula-nvim theme for lualine

--- dot_config/nvim/lua/lualine-config/init.lua
+++ dot_config/nvim/lua/lualine-config/init.lua
@@ -1,6 +1,7 @@
 require'lualine'.setup {
     options = {
         component_separators = { left = '', right = ''},
+        theme = 'dracula-nvim'
     },
 	sections = {
 		lualine_x = {'encoding', 'filetype'},
