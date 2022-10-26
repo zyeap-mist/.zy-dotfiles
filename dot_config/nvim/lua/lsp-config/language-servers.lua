87a26672c7d250c70c7214cdc84d28ef10c28c48
zyeap-mist <100244385+zyeap-mist@users.noreply.github.com>
2022-10-26 14:50:25 -0700

    update_capabilities deprecated

--- dot_config/nvim/lua/lsp-config/language-servers.lua
+++ dot_config/nvim/lua/lsp-config/language-servers.lua
@@ -28,7 +28,7 @@ local on_attach = function(client, bufnr)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 local lsp_flags = {
   -- This is the default in Nvim 0.7+
