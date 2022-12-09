4d3fefe2a75bf0bcddc96a1f09f3757454d3a494
Zach Yeap <zyeap@juniper.net>
2022-12-08 20:24:56 -0800

    Using Nvchad configs

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,13 +1,34 @@
-require('impatient')
-require('settings')
-require('mappings')
-require('packer-config')
-require('tabby-config')
-require('colorschemes-config')
-require('lualine-config')
-require('nvim-tree-config')
-require('nvim-autopairs-config')
-require('lsp-config.language-servers')
-require('lsp-config.nvim-cmp')
-require('lsp-config.null-ls')
-require('treesitter-config')
+vim.defer_fn(function()
+  pcall(require, "impatient")
+end, 0)
+
+require "core"
+require "core.options"
+
+-- setup packer + plugins
+local fn = vim.fn
+local install_path = fn.stdpath "data" .. "/site/pack/packer/opt/packer.nvim"
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e222a" })
+  print "Cloning packer .."
+  fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
+
+  -- install plugins + compile their configs
+  vim.cmd "packadd packer.nvim"
+  require "plugins"
+  vim.cmd "PackerSync"
+
+  -- install binaries from mason.nvim & tsparsers
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "PackerComplete",
+    callback = function()
+      vim.cmd "bw | silent! MasonInstallAll" -- close packer window
+      require("packer").loader "nvim-treesitter"
+    end,
+  })
+end
+
+pcall(require, "custom")
+
+require("core.utils").load_mappings()
